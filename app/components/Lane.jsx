d0316a29e564e7de9998629f5d9fc626a92005ed
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-13 17:14:48 +0100

    Add functionality to edit and delete lanes

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -4,6 +4,7 @@ import Notes from './Notes';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
+import Editable from './Editable';
 
 
 export default class Lane extends Component {
@@ -12,6 +13,7 @@ export default class Lane extends Component {
     super(props);
 
     this.addNote = this.addNote.bind(this);
+    this.editName = this.editName.bind(this);
   }
 
 
@@ -46,17 +48,48 @@ export default class Lane extends Component {
     NoteActions.delete(noteId);
   };
 
+  editName(name) {
+    const laneId = this.props.lane.id;
+
+    console.log(`edit lane ${laneId} name using ${name}`)
+  };
+
+  deleteLane = () => {
+    const laneId = this.props.lane.id;
+
+    console.log(`delete lane ${laneId}`)
+  };
+
+  activateLaneEdit = () => {
+    const laneId = this.props.lane.id;
+
+    console.log(`activate lane ${laneId} edit`)
+  };
+
+  activateNoteEdit = (id) => {
+
+    console.log(`activate note ${id} edit`)
+
+  };
+
   render() {
 
     const {lane, ...props} = this.props;
 
     return (
       <div {...props}>
-        <div className="lane-header">
+        <div className="lane-header" onClick={this.activateLaneEdit}>
           <div className="lane-add-note">
             <button onClick={this.addNote}>+</button>
           </div>
-          <div className="lane-name">{lane.name}</div>
+          <Editable className="lane-name"
+            editing={lane.editing}
+            value={lane.name}
+            onEdit={this.editName}
+          />
+          <div className="lane-delete">
+            <button onClick={this.deleteLane}>x</button>
+          </div>
         </div>
         <AltContainer
           stores={[NoteStore]}
@@ -64,7 +97,11 @@ export default class Lane extends Component {
             notes: () => NoteStore.getNotesByIds(lane.notes)
           }}
         >
-        <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        <Notes
+          onValueClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+        />
         </AltContainer>
       </div>
     );
