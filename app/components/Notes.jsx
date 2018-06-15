7e4a8ece321762d39d3ade6cc24dbf99a23dcd9b
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-15 12:07:03 +0100

    Revert back to babael env, Reinstate Notes and Note changes

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -5,15 +5,18 @@ import Note from './Note';
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map(note =>
-      <Note className="note" id={note.id} key={note.id}>
-          <Editable
-              editing={note.editing}
-              value={note.task}
-              onValueClick={onValueClick.bind(null, note.id)}
-              onEdit={onEdit.bind(null, note.id)}
-              onDelete={onDelete.bind(null, note.id)}
-          />
-      </Note>
-    )}</ul>
+      <Note className="note" id={note.id} key={note.id}
+            onMove={({sourceId, targetId}) =>
+              console.log(`source: ${sourceId}, target: ${targetId}`)
+            }
+      >
+        <Editable
+          editing={note.editing}
+          value={note.task}
+          onValueClick={onValueClick.bind(null, note.id)}
+          onEdit={onEdit.bind(null, note.id)}
+          onDelete={onDelete.bind(null, note.id)}
+        />
+      </Note>    )}</ul>
   );
 }
