c0b9fad25c8c5f0040d33cee7849d376a959a8e8
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-19 16:08:33 +0100

    Add Get Note By Id Test

--- tests/stores/NoteStore.test.js
+++ tests/stores/NoteStore.test.js
@@ -10,31 +10,32 @@ describe('NoteStore', () => {
 
   beforeEach(() => {
     alt.flush();
-    task = "task";
+    task = "test";
+    NoteActions.create({ task: task });
   });
 
   it('should create notes', () => {
-    NoteActions.create({ task });
     const state = NoteStore.getState();
     expect(state.notes[0].task).toEqual(task)
   });
   it('should update notes', () => {
     // const task = 'test';
     const updatedTask = 'test 2';
-    NoteActions.create({task});
     const note = NoteStore.getState().notes[0];
     NoteActions.update({...note, task: updatedTask  });
     const state = NoteStore.getState();
     expect(state.notes[0].task).toEqual(updatedTask);
   });
   it('should delete notes', () => {
-    NoteActions.create({task});
     const note = NoteStore.getState().notes[0];
     NoteActions.delete(note.id);
     const state = NoteStore.getState();
     expect(state.notes.length).toBe(0);
   });
   it('should get notes by Id', () => {
-
+    const note = NoteStore.getState().notes[0];
+    const notes = NoteStore.getNotesByIds([note.id]);
+    expect(notes.length).toBe(1);
+    expect(notes[0].task).toEqual(task);
   });
 });
