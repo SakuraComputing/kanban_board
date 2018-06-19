a42b35fdbb8dfe2d85383e943f292f3e9b6fd24c
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-19 15:58:20 +0100

    Add Update and Delete Note Test

--- tests/stores/NoteStore.test.js
+++ tests/stores/NoteStore.test.js
@@ -6,14 +6,35 @@ import alt from '../../app/libs/alt';
 
 describe('NoteStore', () => {
 
+  let task;
+
   beforeEach(() => {
     alt.flush();
+    task = "task";
   });
 
   it('should create notes', () => {
-    const task = "task";
     NoteActions.create({ task });
     const state = NoteStore.getState();
     expect(state.notes[0].task).toEqual(task)
-  })
+  });
+  it('should update notes', () => {
+    // const task = 'test';
+    const updatedTask = 'test 2';
+    NoteActions.create({task});
+    const note = NoteStore.getState().notes[0];
+    NoteActions.update({...note, task: updatedTask  });
+    const state = NoteStore.getState();
+    expect(state.notes[0].task).toEqual(updatedTask);
+  });
+  it('should delete notes', () => {
+    NoteActions.create({task});
+    const note = NoteStore.getState().notes[0];
+    NoteActions.delete(note.id);
+    const state = NoteStore.getState();
+    expect(state.notes.length).toBe(0);
+  });
+  it('should get notes by Id', () => {
+
+  });
 });
